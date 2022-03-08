f768a564e8f0b249d52b1f9fc478e00af0cfacf2
Gürhan Ustali <gurhanustali@gmail.com>
2022-03-08 19:17:54 +0100

    For Loop

---
+++ src/day06/ForLoop.java
@@ -0,0 +1,60 @@
+package day06;
+
+public class ForLoop {
+    public static void main(String[] args) {
+/*
+        for(int i=0;i<5;i++){
+            System.out.println("Merhaba");
+        }
+*/
+        //Soru; 1'den 10'a kadar olan sayıların toplamını bulan java programını yazınız.
+        //=,+=,-=...
+        //i     -->1 2 3 4 5 6 7 8 9 10 11
+        //toplam-->0 1 3 6 10 15 21 28 36 45 55
+/*
+        int toplam=0;
+        for (int i = 1; i <=10 ; i++) {
+            toplam=toplam+i;
+        }
+        System.out.println("Toplam = "+toplam);
+*/
+
+        //1'den 100'e kadar olan çift sayıların toplamını bulan Java programı yazınız.
+        //int sum=0;
+/*        for (int i = 2; i <=100 ; i=i+2) {
+            sum +=i; //sum=sum+i;
+        }*/
+
+/*        for (int i = 1; i <=100 ; i++) {
+            if (i%2==0){
+                sum=sum+i;
+            }
+        }
+
+        System.out.println(sum);*/
+
+
+        //1'den 5'e kadar olan sayıların çarpımını bulan Java programı yazınız.
+        //carpim    ->1 1 2 6 24 120
+        //i         ->1 2 3 4 5 6
+/*        int carpim=1;
+
+        for (int i = 1; i <=5 ; i++) {
+            carpim=carpim*i;
+        }
+        System.out.println("Çarpım = "+carpim);*/
+
+/*
+        int carpim=1;
+        for (int i = 5; i >=1 ; i--) {
+            carpim *=i;
+        }
+        System.out.println("Çarpım = "+carpim);*/
+
+
+        for (;;){
+            System.out.println("Merhaba");
+        }
+
+    }
+}
---
+++ src/day06/SwitchStatement.java
@@ -0,0 +1,67 @@
+package day06;
+
+public class SwitchStatement {
+
+
+
+    public static void main(String[] args) {
+        //switch .. case
+       // int dayNumber = 17;
+/*        switch (dayNumber){
+            case 1:
+                System.out.println("Pazartesi");break;
+            case 2:
+                System.out.println("Salı");break;
+            case 3:
+                System.out.println("Çarşamba");break;
+            case 4:
+                System.out.println("Perşembe");break;
+            case 5:
+                System.out.println("Cuma");break;
+            case 6:
+                System.out.println("Cumartesi");break;
+            case 7:
+                System.out.println("Pazar");break;
+            default:
+                System.out.println("Yanlış girildi.");break;
+        }*/
+
+/*        switch (dayNumber) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Weekday");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Weekend");
+                break;
+            default:
+                System.out.println("Wrong case");
+                break;
+        }*/
+
+
+        int k1=15;
+        int k2=10;
+
+        switch (k1*k2){
+            case 15:
+                System.out.println("Kargo için uygundur.");
+
+
+
+
+                break;
+            case 20:
+                System.out.println("Kargo için uygun değil");
+                break;
+            default:
+                System.out.println("Girilen verilerle hesaplama yapılamaz.");
+        }
+
+
+    }
+}
